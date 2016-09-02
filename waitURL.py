b57b6c8263fc00e311894bec45aae76f3ac6d4fc
Chad Luo <chad.luo@calibreft.com.au>
2016-09-02 12:04:00 +1000

    remove external dependency

--- waitURL.py
+++ waitURL.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+
 import os
-import requests
 import sys
 import time
+import urllib2
 
 timeSoFar = 0
 waitTime = 5
@@ -16,22 +18,23 @@ command = sys.argv[3:]
 
 print("Attempting to connect to URL: %s and then run command %s" % (url, command))
 
-while True and timeSoFar < timeOut:
+req = urllib2.Request(url)
+
+while timeSoFar < timeOut:
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
+        res = urllib2.urlopen(req)
+
+    except (urllib2.HTTPError, urllib2.URLError) as e:
+        timeSoFar = timeSoFar + waitTime
+        time.sleep(waitTime)
+
+    else:
+        if res.code == 200:
             print("Connected to URL %s" % url)
             os.system(" ".join(command))
             break
         else:
-            print("URL %s not available with status code of %s" % (url, response.status_code))
             timeSoFar = timeSoFar + waitTime
             time.sleep(waitTime)
-
-    except:
-        e = sys.exc_info()[0]
-        print("Unable to connect to URL %s due to: %s" % (url, e))
-        timeSoFar = timeSoFar + waitTime
-        time.sleep(waitTime)
 
 print("waitURL completed for %s " % url)
